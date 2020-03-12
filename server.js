2f38f3ed7eba07e5475e4e8c8d408bcd3e3738d9
Steven Weatherspoon <stvnwthrsp@gmail.com>
2020-03-11 20:00:23 -0400

    Added public folder and index.html

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// All files in the folder "public" will be accessible by the application
+app.use(express.static('public'));
+
+// Send index.html at the root URL
 app.get('/', (req, res) => {
-  res.write("<html>");
-  res.write("<body>");
-  res.write("<h1>Page Title</h1>");
-  res.write("</body>");
-  res.write("</html>");
-  res.end();
+  res.sendFile("index.html");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
